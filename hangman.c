a135eeacfc611085d3b496d50e182908f39881ac
@jmarcos-cano <jmarcos.cano@gmail.com>
2020-02-19 17:38:16 -0600

    @jmarcos-cano - stringcompare

--- hangman.c
+++ hangman.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 // I need string functions, strcpy
+// https://www.programiz.com/c-programming/library-function/string.h
 #include <string.h>
 #include <time.h>
 
@@ -21,6 +22,7 @@ const char* get_random_word(void);
 // este sera nuestro banco de palabras, por favor agregue, quite, modifique las palabras que quiera
 // puede agregar hasta un maximo de MAX_WORDS palabras, si desea agregar mas de 23 palabras
 // modifique MAX_WORDS a la cantidad que se ajuste a sus necesidades.
+// ! NO AGREGUE PALABRAS QUE CONTENGAN NUMEROS
 // ? este es un arreglo de strings, un arreglo de punteros ;)
 const char *word_collection[MAX_WORDS]= {
                     "este","es","un","banco","de",
@@ -44,6 +46,7 @@ const char* get_random_word(void){
 }
 
 
+
 /*
 Insert your code here
 */
@@ -52,6 +55,11 @@ int main(){
     // note como se invoca la funcion, la funcion se invoca de esta manera y se guarda en una variable
     const char* palabra_elegida = get_random_word();
     printf("%s\n",palabra_elegida);
+
+    // comparemos 2 strings. puede remover estas lineas.
+    // char str1[] = "abcd", str2[] = "abcd", str3[]="abCd";
+    // printf("strcmp(str1, str2) = %d\n", strcmp(str1,str2));
+    // printf("strcmp(str1, str2) = %d\n", strcmp(str1,str3));
 
     /*
         Insert your code here
