11b769da5f6f9419b3c21293a18463419a7fcf54
Luis Venegas <luisvenegas@ufm.edu>
2020-03-06 12:23:04 -0600

    Minor changes

--- hangman.c
+++ hangman.c
@@ -97,6 +97,7 @@ int juego(void){
     char palabra[50];
     const char* random_word = get_random_word();
 
+
     // gets random word from word bank, copies it to random_word, gets length
     strcpy(palabra, random_word);
     length = strlen(palabra);
@@ -114,11 +115,13 @@ int juego(void){
     printf("\n\n");
 
     // Main body of the function, runs (intentos) times, asking for a letter each time.
-    while(tries < intentos && strcmp(underscored, palabra) != 0 && buscar != '0') {
+    while(tries < intentos && strcmp(underscored, palabra) != 0 && buscar != '0' && aciertos != length) {
+        printf("\033[1;36m");
         printf("Jugador: "); fputs(nombre, stdout);
         printf("Intentos: %d de %d\n", tries, intentos);
         printf("Aciertos: %d\n", aciertos);
         printf("Palabra: \n\n");
+        printf("\033[0m");
 
         // Prints underscored.
         for (int i = 0; i < length; i++) {
@@ -143,7 +146,7 @@ int juego(void){
 
     printf("\n\n");
     // Checks if you guessed or not.
-    if(strcmp(underscored, palabra) == 0){
+    if(strcmp(underscored, palabra) == 0 || aciertos == length){
         puts(palabra);
 
         //Aqui va el menu final ganador, Intentos, nombre, palabra: Y el ganaste
@@ -163,7 +166,7 @@ int menu_final(int gano){
         printf("Ganaste\nPresione ENTER para regresar al menu\n");
         fgets(nothing, 3, stdin);
         fgets(nothing, 3, stdin);
-        //clearscreen(); didnt work
+        clearscreen();
 
     }
     else if(gano == 0){
@@ -215,8 +218,8 @@ int main(){
 // Trabajo por hacer: 
 /*
  * Menu final ganaste / perdiste
- * Limpiar la pantalla, sea con comandos de terminal o lineas nuevas
 
+ * Limpiar la pantalla, sea con comandos de terminal o lineas nuevas TERMINADO !!!
  * Checkear el input del menu principal y asegurar que solo se puedan ingresar numeros TERMINADO!!!
  * Salir del juego al presionar 0 TERMINADO!!
  *
